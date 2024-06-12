549d8e9a142421eeea6a0fc3d680c2b374638166
kologermit <kologermit@gmail.com>
2024-06-12 22:01:18 +0400

    fix 3

--- handlers.py
+++ handlers.py
@@ -334,18 +334,6 @@ def theory_cb(bot: TeleBot, callback: types.CallbackQuery, user: Model, db_manag
         if theory.rowid not in user.accepted_theory:
             user.accepted_theory.append(theory.rowid)
             db_manager.save_data(user)
-        theorys = db_manager.find_data(TheoryModel)
-        print(theorys)
-        print(theory.rowid)
-        if len(theorys) < theory.rowid:
-            theory = theorys[theory.rowid]
-            confirm_theory = "<b>Теория пройдена✅</b>\n"
-            message = f"""<b>Следующая теория</b>\n\\
-{confirm_theory if theory.rowid in user.accepted_theory else ''}\\
-<b>{theory.name}</b>\n{theory.description}"""
-            markup = types.InlineKeyboardMarkup()
-            markup.add(types.InlineKeyboardButton("Изучить теорию", callback_data=json.dumps({"c": "theory", "id": theory.rowid})))
-            send_description(bot, user, message, theory.files, markup)
         return to_menu(bot, message, user, db_manager, theory_complete)
     
 def theory_next_cb(bot: TeleBot, callback: types.CallbackQuery, user: Model, db_manager: DBManager):
@@ -377,6 +365,16 @@ def theory_next_cb(bot: TeleBot, callback: types.CallbackQuery, user: Model, db_
         description=next_paragraph["description"])
     message = send_description(bot, user, message, next_paragraph["files"], markup)
     if len(theory.paragraphs) == next_paragraph["id"]+1:
+        theorys = db_manager.find_data(TheoryModel)
+        if len(theorys) < theory.rowid:
+            theory = theorys[theory.rowid]
+            confirm_theory = "<b>Теория пройдена✅</b>\n"
+            message = f"""<b>Следующая теория</b>\n\\
+{confirm_theory if theory.rowid in user.accepted_theory else ''}\\
+<b>{theory.name}</b>\n{theory.description}"""
+            markup = types.InlineKeyboardMarkup()
+            markup.add(types.InlineKeyboardButton("Изучить теорию", callback_data=json.dumps({"c": "theory", "id": theory.rowid})))
+            send_description(bot, user, message, theory.files, markup)
         return to_menu(bot, message, user, db_manager, theory_complete)
 
 def theory_back_cb(bot: TeleBot, callback: types.CallbackQuery, user: Model, db_manager: DBManager):
